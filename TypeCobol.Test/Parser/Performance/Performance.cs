593789d915f90f8d28432b0f22a58bafb5ef1f48
osmedile <osmedile@gmail.com>
2019-03-02 22:49:08 +0100

    WI #1296 Add perf tests
    Cobol85 that serves as reference
    Cobol85 pgm with big declaration transformed to TypeCobol.
    Simply replaced Copy with typedef.
    
    Cobol85 pgm with working-storage vars moved to global-storage
    Cobol85 with typedef and all code moved into a single procedure
    
    1 unsused big type usage that serves as reference
    100 unused big type usage to compare

--- TypeCobol.Test/Parser/Performance/Performance.cs
+++ TypeCobol.Test/Parser/Performance/Performance.cs
@@ -16,8 +16,76 @@ namespace TypeCobol.Test.Parser.Performance
     {
         static readonly string AntlrFolder = PlatformUtils.GetPathForProjectFile("Parser") + Path.DirectorySeparatorChar + "Performance";
 
+
+        private static readonly string CNAF_FOLDER = "TypeCobol.Test" + Path.DirectorySeparatorChar + "ThirdParty" + Path.DirectorySeparatorChar + "CNAF" + Path.DirectorySeparatorChar + "Batch" + Path.DirectorySeparatorChar;
+        private static readonly string CNAF_TC_FOLDER = "TypeCobol.Test" + Path.DirectorySeparatorChar + "ThirdParty" + Path.DirectorySeparatorChar + "CNAF_TypeCobol" + Path.DirectorySeparatorChar;
+
+
+        /// <summary>
+        /// It's under CNAF_TC_FOLDER because the file was modified for the performance test.
+        /// All redefines have been removed.
+        ///
+        /// It's because others test need to simulate the same code with 
+        /// typedef but redefines is forbidden under typedef.
+        /// 
+        /// With this program we can compare performance pure Cobol85 and a program written with
+        /// TypeCobol features(typedef, procedure, global-storage, operator ::, ...)
+        /// </summary>
+        private static readonly string Cobol85 = CNAF_TC_FOLDER + "CGMV01-Cobol85-NoRedefines.cbl";
+
+        /// <summary>
+        /// Almost like Cobol85 file but replace almost level 01 declaration with a typedef.
+        /// It's like replacing COPY with typedef
+        ///
+        /// Performance should be similar to Cobol85
+        /// </summary>
+        private static readonly string BigTypes_NoProcedure = CNAF_TC_FOLDER + "CGMV01-BigTypes.tcbl";
+
+        /// <summary>
+        /// This file is almost like BigTypes_NoProcedure except all the code is put in a procedure/function instead of the
+        /// procedure division of the program.
+        ///
+        /// The goal is to be sure that no checkers (or any code) make extra works when we are under a procedure/function.
+        ///
+        /// Performance should be similar to BigTypes_NoProcedure
+        /// 
+        /// </summary>
+        private static readonly string BigTypes_1Procedure = CNAF_TC_FOLDER + "CGMV01-BigTypes-1Procedure.tcbl";
+
+        /// <summary>
+        /// Performance should be similar to BigTypes_NoProcedure
+        ///
+        /// This file is almost like Cobol85 except all variables are declared in Global-storage.
+        /// Linkage section is still here
+        ///
+        /// The goal is to be sure that no checkers (or any code) make extra works when we are under the global-storage
+        ///
+        /// Performance should be similar to Cobol85
+        /// </summary>
+        private static readonly string GlobalStorage = CNAF_TC_FOLDER + "CGMV01-GlobalStoragey.tcbl";
+
+        /// <summary>
+        /// With TypeCobol, the goal is to have a lot of small procedures that communicates with variable declared in type.
+        /// We'll then have the same type use a lot of times in different procedures.
+        ///
+        /// The goal of this file is to check that there is no performance penalty by declaring a lot of variable with a type
+        /// The code doesn't use the variable, we just check the declaration part
+        ///
+        /// This file only 1 time the type.
+        /// It serves as a reference for the other test
+        /// </summary>
+        private static readonly string UseALotOfTypes_1Times_Reference = CNAF_TC_FOLDER + "CGMV01-UseAlotOfTypes-1Times.tcbl";
+
+        /// <summary>
+        /// Same as UseALotOfTypes_1Times_Reference but declare a variable with a type 100 times
+        /// </summary>
+        private static readonly string UseALotOfTypes_100Times = CNAF_TC_FOLDER + "CGMV01-UseAlotOfTypes-100Times.tcbl";
+
+
+
+
         [TestMethod]
-        [TestCategory("Parsing")]
+        [TestCategory("Performance")]
         [TestProperty("Time", "fast")]
         public void AntlrPerformanceProfiler()
         {
@@ -31,38 +99,75 @@ namespace TypeCobol.Test.Parser.Performance
 
 
         [TestMethod]
-        [TestCategory("Incremental")]
+        [TestCategory("Performance")]
         [TestProperty("Time", "long")]
-        [Ignore]
-        public void IncrementalPerformance()
+        public void Incremental_Cobol85_NoRedefines()
         {
-            IncrementalPerformance2(PlatformUtils.GetPathForProjectFile("ThirdParty" + Path.DirectorySeparatorChar + "CNAF" + Path.DirectorySeparatorChar + "Batch"), "CGMV01.COB",
-                 55987, "           MOVE LOW-VALUE     TO C1M010R.                               CGZPARAK");
+            IncrementalPerformance2(Cobol85, 65811, "           MOVE WS-CMM010-MOIS-BIN TO WS-CMM010-MM                      CMM010AK");
         }
 
-        private void IncrementalPerformance2(string folder, string textName, int newLineIndex, string newLineText)
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void Incremental_TC_BigTypesNoProcedure()
+        {
+            IncrementalPerformance2(BigTypes_NoProcedure, 65897, "           MOVE WS-CMM010-MOIS-BIN TO WS-CMM010-MM                      CMM010AK");
+        }
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void Incremental_TC_BigTypesWithProcedure()
+        {
+            IncrementalPerformance2(BigTypes_1Procedure,65899, "           MOVE WS-CMM010-MOIS-BIN TO WS-CMM010-MM                      CMM010AK");
+        }
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void Incremental_TC_GlobaStorage()
+        {
+            IncrementalPerformance2(BigTypes_1Procedure,65807, "           MOVE WS-CMM010-MOIS-BIN TO WS-CMM010-MM                      CMM010AK");
+        }
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void Incremental_TC_UseALotOfTypes_1Times_Reference()
+        {
+            IncrementalPerformance2(UseALotOfTypes_1Times_Reference,50, "                                                                                ");
+        }
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void Incremental_TC_UseALotOfTypes_100Times()
+        {
+            IncrementalPerformance2(UseALotOfTypes_100Times,50, "                                                                                ");
+        }
+
+        private void IncrementalPerformance2(string relativePath, int newLineIndex, string newLineText)
         { 
             DocumentFormat documentFormat = DocumentFormat.RDZReferenceFormat;
+            string fullPath = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.FullName + "\\" + relativePath;
 
             // Create a FileCompiler for this program
-            DirectoryInfo localDirectory = new DirectoryInfo(folder);
-            if (!localDirectory.Exists)
-            {
-                throw new Exception(String.Format("Directory : {0} does not exist", localDirectory.FullName));
-            }
+            string filename = Path.GetFileName(fullPath);
+            var root = new DirectoryInfo(Directory.GetParent(fullPath).FullName);
+
             CompilationProject project = new CompilationProject("test",
-                localDirectory.FullName, new string[] { ".cbl", ".cpy" },
+                root.FullName, new[] { ".cbl", ".cpy" },
                 documentFormat.Encoding, documentFormat.EndOfLineDelimiter, documentFormat.FixedLineLength, documentFormat.ColumnsLayout, new TypeCobolOptions());
-            FileCompiler compiler = new FileCompiler(null, textName, project.SourceFileProvider, project, documentFormat.ColumnsLayout, new TypeCobolOptions(), null, false, project);
+            FileCompiler compiler = new FileCompiler(null, filename, project.SourceFileProvider, project, documentFormat.ColumnsLayout, new TypeCobolOptions(), null, false, project);
             //Make an incremental change to the source code
             TestUtils.CompilationStats stats = new TestUtils.CompilationStats();
-            ExecuteInceremental(compiler, stats, newLineIndex, newLineText);
+            ExecuteIncremental(compiler, stats, newLineIndex, newLineText);
 
             // Display a performance report
             TestUtils.CreateRunReport("Incremental", TestUtils.GetReportDirectoryPath(), compiler.CobolFile.Name, stats, compiler.CompilationResultsForProgram);
         }
 
-        private static void ExecuteInceremental(FileCompiler compiler, TestUtils.CompilationStats stats, int newLineIndex, string newLineText )
+        private static void ExecuteIncremental(FileCompiler compiler, TestUtils.CompilationStats stats, int newLineIndex, string newLineText )
         {
             // Execute a first (complete) compilation
             compiler.CompileOnce();
@@ -103,18 +208,63 @@ namespace TypeCobol.Test.Parser.Performance
                                                stats.AverateTemporarySemanticsParserTime;
         }
 
+
         [TestMethod]
-        [TestCategory("Parsing")]
+        [TestCategory("Performance")]
         [TestProperty("Time", "long")]
-        [Ignore]
-        public void FullParsingAndGenerationTest()
+        public void FullParsing_Cobol85_NoRedefines()
         {
-            string pwd = Directory.GetCurrentDirectory();
-            FullParsingAndGenerationTest2(Directory.GetParent(pwd)?.Parent?.FullName + "\\TypeCobol.Test\\ThirdParty\\CNAF\\Batch\\CGMV01.COB");
+            FullParsing(Cobol85);
         }
 
-        private void FullParsingAndGenerationTest2(string path, params string[] copiesFolder)
-        { 
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void FullParsing_TC_BigTypesNoProcedure()
+        {
+            FullParsing( BigTypes_NoProcedure);
+        }
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void FullParsing_TC_BigTypesWithProcedure()
+        {
+            FullParsing( BigTypes_1Procedure);
+        }
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void FullParsing_TC_GlobalStorage()
+        {
+            FullParsing(GlobalStorage);
+        }
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void FullParsing_TC_UseALotOfTypes_1Times_Reference()
+        {
+            FullParsing(UseALotOfTypes_1Times_Reference);
+        }
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        [TestProperty("Time", "long")]
+        public void FullParsing_TC_UseALotOfTypes_100Times()
+        {
+            FullParsing(UseALotOfTypes_100Times);
+        }
+
+
+
+
+
+
+
+        private void FullParsing(string relativePath, params string[] copiesFolder)
+        {
+            string fullPath = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.FullName + "\\" + relativePath;
             var format = TypeCobol.Compiler.DocumentFormat.RDZReferenceFormat;
 
             TestUtils.CompilationStats stats = new TestUtils.CompilationStats();
@@ -134,8 +284,8 @@ namespace TypeCobol.Test.Parser.Performance
             {
 
                 documentWarmup = new TypeCobol.Parser();
-                documentWarmup.Init(path, optionsWarmup, format, copiesFolder);
-                documentWarmup.Parse(path);
+                documentWarmup.Init(fullPath, optionsWarmup, format, copiesFolder);
+                documentWarmup.Parse(fullPath);
 
                 var document = new TypeCobol.Parser();
                 var options = new TypeCobolOptions
@@ -145,8 +295,8 @@ namespace TypeCobol.Test.Parser.Performance
                     AutoRemarksEnable = true
 #endif
                 };
-                document.Init(path, options, format, copiesFolder);
-                document.Parse(path);
+                document.Init(fullPath, options, format, copiesFolder);
+                document.Parse(fullPath);
                 stats.AverageTextUpdateTime += document.Results.PerfStatsForText.FirstCompilationTime;
                 stats.AverageScannerTime += document.Results.PerfStatsForScanner.FirstCompilationTime;
                 stats.AveragePreprocessorTime += document.Results.PerfStatsForPreprocessor.FirstCompilationTime;
@@ -174,7 +324,7 @@ namespace TypeCobol.Test.Parser.Performance
             stats.TotalCodeElements = documentWarmup.Results.CodeElementsDocumentSnapshot.CodeElements.Count();
 
 
-            TestUtils.CreateRunReport("FullParsing", TestUtils.GetReportDirectoryPath(), Path.GetFileNameWithoutExtension(path), stats);
+            TestUtils.CreateRunReport("FullParsing", TestUtils.GetReportDirectoryPath(), Path.GetFileNameWithoutExtension(fullPath), stats);
         }
     }
 }
